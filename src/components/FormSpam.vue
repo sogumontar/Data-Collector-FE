bae9e1d1a35c8ad4c82644a33b71e5fc71bbe4c3
Hendra Simangunsong <hendrasimz92@gmail.com>
2021-03-02 00:27:51 +0800

    fixing form spam

--- src/components/FormSpam.vue
+++ src/components/FormSpam.vue
@@ -1,64 +1,68 @@
 <template>
     <div>
-    <h1 align="center">LAPORKAN SMS SPAM</h1>
-        <div class="container card" style="padding: 50px; margin-bottom: 43px">
-            <v-form v-model="valid">
-                <v-row>
-                    <v-col cols="12" md="12">
-                        <v-text-field
-                                type="number"
-                                v-model="nomor"
-                                :counter="13"
-                                label="Nomor Pengirim Pesan"
-                                required>
-                        </v-text-field>
-                    </v-col>
-                    <v-col cols="12" md="12">
-                        <v-text-field
-                                v-model="isi"
-                                label="Isi Pesan"
-                                required>
-                        </v-text-field>
-                    </v-col>
-                    <!--                <v-col cols="12" md="4">-->
-                    <!--                    <v-text-field-->
-                    <!--                            type="number"-->
-                    <!--                            v-model="jumlah"-->
-                    <!--                            label="Jumlah"-->
-                    <!--                            min="1"-->
-                    <!--                            required>-->
-                    <!--                    </v-text-field>-->
-                    <!--                </v-col>-->
-                    <v-col cols="12" md="6">
-                        <v-select
-                                v-model="kategori_select"
-                                :items="kategori"
-                                :rules="[v => !!v || 'Item is required']"
-                                label="Kategori Pesan"
-                                required>
-                        </v-select>
-                    </v-col>
-                </v-row>
-                <v-row>
+        <fieldset class="field_set card">
+            <legend align="center"><b>LAPORKAN SMS SPAM</b></legend>
+            <div class="container " style="padding: 50px; margin-bottom: 43px">
+                <v-form v-model="valid">
+                    <v-row>
+                        <v-col cols="12" md="12">
+                            <v-text-field
+                                    type="number"
+                                    v-model="nomor"
+                                    :counter="13"
+                                    label="Nomor Pengirim Pesan"
+                                    required>
+                            </v-text-field>
+                        </v-col>
+                        <v-col cols="12" md="12">
+                            <v-text-field
+                                    v-model="isi"
+                                    label="Isi Pesan"
+                                    required>
+                            </v-text-field>
+                        </v-col>
+                        <!--                <v-col cols="12" md="4">-->
+                        <!--                    <v-text-field-->
+                        <!--                            type="number"-->
+                        <!--                            v-model="jumlah"-->
+                        <!--                            label="Jumlah"-->
+                        <!--                            min="1"-->
+                        <!--                            required>-->
+                        <!--                    </v-text-field>-->
+                        <!--                </v-col>-->
+                        <v-col cols="12" md="6">
+                            <v-select
+                                    v-model="kategori_select"
+                                    :items="kategori"
+                                    :rules="[v => !!v || 'Item is required']"
+                                    label="Kategori Pesan"
+                                    required>
+                            </v-select>
+                        </v-col>
+                    </v-row>
+                    <v-row>
 
-                    <v-col cols="12" md="6">
-                        <v-text-field
-                                id="tanggal"
-                                v-model="tanggal"
-                                type="date"
-                                label="Tanggal Pesan Diterima"
-                                required>
-                        </v-text-field>
-                    </v-col>
-                </v-row>
-                <div align="right">
-                    <v-btn color="#0560AA" style="color: white; alignment: right" class="mr-4" @click="submit">
-                        submit
-                    </v-btn>
-                </div>
-            </v-form>
-            <br>
-        </div>
+                        <v-col cols="12" md="6">
+                            <v-text-field
+                                    id="tanggal"
+                                    v-model="tanggal"
+                                    type="date"
+                                    label="Tanggal Pesan Diterima"
+                                    required>
+                            </v-text-field>
+                        </v-col>
+                    </v-row>
+                    <div align="right">
+                        <v-btn color="#0560AA" style="color: white; alignment: right" class="mr-4"
+                               @click="submit">
+                            submit
+                        </v-btn>
+                    </div>
+                </v-form>
+                <br>
+            </div>
+        </fieldset>
+        <br><br>
     </div>
 </template>
 
@@ -146,6 +150,17 @@
     }
 </script>
 
-<style scoped>
-
+<style>
+    .field_set {
+        border-color: #000;
+        border-style: solid;
+        display: block;
+        margin-left: 2px;
+        margin-right: 2px;
+        padding-top: 0.35em;
+        padding-bottom: 0.625em;
+        padding-left: 0.75em;
+        padding-right: 0.75em;
+        border: 2px groove (internal value);
+    }
 </style>
